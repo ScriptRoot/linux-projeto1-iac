4dded6050c81d3f09726d41fe948bd5f1dc558ea
ScriptRoot <vezu04@gmail.com>
2024-11-17 18:18:56 -0300

    Onde irei salvar os scripts do curso de Linux

---
+++ criar_usuarios_em_lote.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+echo "Criando diretórios..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando grupos de usuários..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários..."
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+
+
+echo "Atribuindo permissões..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Configuração de senhas padrão..."
+
+echo "carlos:Senha123" | chpasswd
+echo "maria:Senha123" | chpasswd
+echo "joao:Senha123" | chpasswd
+echo "debora:Senha123" | chpasswd
+echo "sebastiana:Senha123" | chpasswd
+echo "roberto:Senha123" | chpasswd
+echo "josefina:Senha123" | chpasswd
+echo "amanda:Senha123" | chpasswd
+echo "rogerio:Senha123" | chpasswd
+
+echo "Configurações realizadas com sucesso!"
